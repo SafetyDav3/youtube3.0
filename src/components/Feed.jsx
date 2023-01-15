38fee2d6315c37b04351e7456f2e8f9ec4c1f8a4
SafteyDav3 <talktomedw@gmail.com>
2023-01-15 16:36:31 -0600

    Working on FEED

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: {sx:'column', md:'row'}}}>
+      <Box sx={{ height: {sx:'auto', md:'92vh'}, borderRight:'1px solid #3d3d3d', px: { sx: 0, md:2}}}>
+
+      </Box>
+    </Stack>
   )
 }
 
