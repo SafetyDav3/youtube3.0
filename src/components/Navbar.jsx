3e851de747bf0579c6c6529c23b53ed15a07a7e3
SafteyDav3 <talktomedw@gmail.com>
2023-01-15 16:26:20 -0600

    search bar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import { Stack } from '@mui/material'
-import { Link } from 'react-router-dom'
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        background: "#000",
+        top: 0,
+        justifyContent: "space-between",
+      }}
+    >
+      <Link>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
 
-export default Navbar
+      <SearchBar/>
+
+    </Stack>
+  );
+};
+
+export default Navbar;
